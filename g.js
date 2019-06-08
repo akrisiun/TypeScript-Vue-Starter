73ebd5329ee871fe3d32f3fe0938643294a9c651
AndriusK <andrius.k@sanitex.eu>
2019-06-08 22:55:25 +0300

    with vue 2.5.2 , webpack 4.x, tsc 2.7.x - success!

---
+++ g.js
@@ -0,0 +1,18 @@
+// global
+
+var g;
+// This works in non-strict mode
+g = (function() {
+	return this;
+})();
+
+try {
+	// This works if eval is allowed (see CSP)
+	g = g || new Function("return this")();
+} catch (e) {
+	// This works if the window reference is available
+	if (typeof window === "object") g = window;
+}
+
+// g can still be undefined, but nothing to do about it...
+module.exports = g;
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,33 @@
 
-var path = require('path')
-var webpack = require('webpack')
+const path = require('path')
+const webpack = require('webpack')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = {
+  mode: 'development',
+  context: __dirname,
   entry: './src/index.ts',
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
     filename: 'build.js'
   },
+  plugins:[
+    new VueLoaderPlugin()
+  ],
   module: {
     rules: [
+	  { test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ],
+      },
       {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: {
           loaders: {
-            // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
-            // the "scss" and "sass" values for the lang attribute to the right configs here.
             // other preprocessors should work out of the box, no loader config like this necessary.
             'scss': 'vue-style-loader!css-loader!sass-loader',
             'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax',
@@ -43,7 +53,7 @@ module.exports = {
     ]
   },
   resolve: {
-    extensions: ['.ts', '.js', '.vue', '.json'],
+    extensions: ['.ts', '.vue', '.js', '.json'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js'
     }
